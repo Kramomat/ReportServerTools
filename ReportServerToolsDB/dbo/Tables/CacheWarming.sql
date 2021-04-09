c7f61908f376f00f00ecedfd5fa971a68dc43208
Mark Kuschel <mark.kuschel@drjve.de>
2021-04-09 12:17:08 +0200

    First Version of ReportServerTools

---
+++ ReportServerToolsDB/dbo/Tables/CacheWarming.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[CacheWarming] (
+    [ItemID]               UNIQUEIDENTIFIER NOT NULL,
+    [Path]                 NVARCHAR (425)   COLLATE Latin1_General_100_CI_AS_KS_WS NOT NULL,
+    [Name]                 NVARCHAR (425)   COLLATE Latin1_General_100_CI_AS_KS_WS NOT NULL,
+    [ParentID]             UNIQUEIDENTIFIER NULL,
+    [ModifiedDate]         DATETIME         NOT NULL,
+    [ActivateCacheWarming] BIT              CONSTRAINT [DF_CacheWarming_ActivateCacheWarming] DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_CacheWarming] PRIMARY KEY CLUSTERED ([ItemID] ASC)
+);
+
---
+++ ReportServerToolsDB/dbo/Tables/CacheWarmingLog.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[CacheWarmingLog] (
+    [CacheWarmingLogID] INT              IDENTITY (1, 1) NOT NULL,
+    [ItemID]            UNIQUEIDENTIFIER NOT NULL,
+    [StartTime]         DATETIME         NOT NULL,
+    [EndTime]           DATETIME         NULL,
+    [Status]            TINYINT          NOT NULL,
+    [Duration]          SMALLINT         NULL,
+    [Details]           VARCHAR (4000)   NULL,
+    CONSTRAINT [PK_CacheWarmingLog] PRIMARY KEY CLUSTERED ([CacheWarmingLogID] ASC)
+);
+
