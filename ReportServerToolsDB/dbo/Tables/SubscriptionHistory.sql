81c8739d00ccd0819dca429568a8fa0d2f024291
Mark Kuschel <mark.kuschel@drjve.de>
2021-04-09 12:22:02 +0200

    Tables Added, Changed Standard Startup Project

---
+++ ReportServerToolsDB/dbo/Tables/SubscriptionHistory.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[SubscriptionHistory] (
+    [SubscriptionHistoryID] BIGINT           NOT NULL,
+    [SubscriptionID]        UNIQUEIDENTIFIER NOT NULL,
+    [Type]                  TINYINT          NULL,
+    [StartTime]             DATETIME         NULL,
+    [EndTime]               DATETIME         NULL,
+    [Status]                TINYINT          NULL,
+    [Message]               NVARCHAR (1500)  COLLATE Latin1_General_100_CI_AS_KS_WS NULL,
+    [Details]               NVARCHAR (4000)  COLLATE Latin1_General_100_CI_AS_KS_WS NULL,
+    CONSTRAINT [PK_SubscriptionHistory] PRIMARY KEY CLUSTERED ([SubscriptionHistoryID] ASC)
+);
+
